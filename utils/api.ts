bff4cb36695c34a011b5dcb91d8a481e595e6d91
thaythienit <thienhv.daknong@gmail.com>
2025-11-01 17:59:58 +0700

    Add files via upload

---
+++ utils/api.ts
@@ -0,0 +1,31 @@
+
+import axios from 'axios';
+
+const api = axios.create({
+  // For local development, this points to your backend server.
+  // When deploying, you will need to change this to your live backend URL from Render.
+  baseURL: 'http://localhost:3001/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor to include the token in headers
+api.interceptors.request.use(
+  (config) => {
+    // Get user from localStorage
+    const userString = localStorage.getItem('school-portal-user');
+    if (userString) {
+      const user = JSON.parse(userString);
+      if (user && user.token) {
+        config.headers.Authorization = `Bearer ${user.token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
