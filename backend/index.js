06e5761ae537bd58bc2ba77277f30f15fa1d67e7
thaythienit <thienhv.daknong@gmail.com>
2025-11-01 20:55:00 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fileRoutes = require('./routes/files.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
